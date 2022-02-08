f98d2e20f669aa06797a17fe7df3dbc021973850
IshwarPatro <ishwarpatro000@gmail.com>
2022-02-08 23:53:21 +0530

    fifth

--- index.js
+++ index.js
@@ -22,7 +22,35 @@ console.log(a);
 var str="how are you doing?\nI am fine.\nHow about you."
 console.log(str)
 
+
 // Input variable in output
 
 var num=200.8
 console.log(`half of ${num} is ${num/2}`)
+
+// var vs let
+
+let c=100
+console.log(c)
+
+// let a="123"
+// console.log(a)
+// let a=100
+//     ^
+
+// SyntaxError: Identifier 'a' has already been declared
+
+// For loop in Js
+
+var num=10;
+console.log("Print even number 0-10:-")
+for(var i=0;i<=num;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
+// Const
+
+const b=90;
+console.log(b)
