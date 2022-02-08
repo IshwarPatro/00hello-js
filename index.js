9bd235aa1575b511e79a6d789df524ae3fc80bde
IshwarPatro <ishwarpatro000@gmail.com>
2022-02-08 23:05:21 +0530

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+var a;
+
+a=10;
+console.log(typeof a);
+console.log(a);
+
+a="hello world";
+console.log(typeof a);
+console.log(a);
+
+a=true;
+console.log(typeof a);
+console.log(a);
+
+a=null
+console.log(typeof a);
+console.log(a);
+
