fde05d8ab185af744417cace8c34625960d48db9
IshwarPatro <ishwarpatro000@gmail.com>
2022-02-12 23:51:47 +0530

    Eight

--- function.js
+++ function.js
@@ -38,3 +38,18 @@ let sayHi=function () {
 console.log("line 54\n" + sayHi);
 sayHi();
 //abcd(); // error abcd is not defined
+
+
+// 3) IIFE-> Imediately invoke function expression
+
+function add(a, b) {
+    return a + b
+}
+add(2, 3);
+
+let additionIIFE = (function (a, b) {
+  console.log(a + b);
+})(20, 30);
+
+// console.log(additionIIFE);
+// console.log(additionIIFE(20, 30));
